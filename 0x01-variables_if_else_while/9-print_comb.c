1985414062534c0179b83b324fe742a4d65c600e
Walingua <waleade77@gmail.com>
2022-09-11 13:27:23 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - my entry point
+ *
+ * Description: code to print combination
+ * of single digit
+ * Return: 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
