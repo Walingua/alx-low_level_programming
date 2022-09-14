aaa2ba2e561dc3c21ed35a38bf6bf68486540a60
Walingua <waleade77@gmail.com>
2022-09-14 16:17:29 -0700

    a + b

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,75 +1,34 @@
 #include "main.h"
-
 /**
- *
- *  * times_table - prints a nine by nine times table
- *
- *   * Return: none
- *
- *    */
-
-
-
+ * times_table - prints a nine by nine times table
+ * Return: none
+ */
 void times_table(void)
-
 {
-
-		int i, j, k;
-
-
-
-			for (i = 0; i <= 9; i++)
-
-					{
-
-								for (j = 0; j <= 9; j++)
-
-											{
-
-															k = i * j;
-
-																		if (j == 0)
-
-																						{
-
-																											_putchar(k + '0');
-
-																														}
-
-																					if (k < 10 && j != 0)
-
-																									{
-
-																														_putchar(',');
-
-																																		_putchar(' ');
-
-																																						_putchar(' ');
-
-																																										_putchar(k + '0');
-
-																																													} else if (k >= 10)
-
-																																																	{
-
-																																																						_putchar(',');
-
-																																																										_putchar(' ');
-
-																																																														_putchar((k / 10) + '0');
-
-																																																																		_putchar((k % 10) + '0');
-
-																																																																					}
-
-																																															}
-
-										_putchar('\n');
-
-											}
-
+	int i, j, k;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}																	if (k < 10 && j != 0)
+			{
+				_putchar(',');
+			 	_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+		 	{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
-
-Footer
-
-
