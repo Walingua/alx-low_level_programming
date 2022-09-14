a8222f7349fd607758c1d1129123bea3ce7a2955
Walingua <waleade77@gmail.com>
2022-09-14 05:41:13 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include "main.h"
+
 /**
  * _putchar - writes the character c to stdout
  * @c: the character to print
@@ -7,25 +7,6 @@
  * on error, -1 is returned and error is set appropriately.
  */
 
-int main(void){
-
-	char ch[] = "_putchar";
-		int i = 0;
-		char c;
-
-		while (i <= 8){
-			c = ch[i];
-			_putchar(c);
-			i++;
-		
-		
-		
-		
-		}
-		_putchar('\n');
-		return(0);
-
-}
 
 int _putchar(char c)
 {
