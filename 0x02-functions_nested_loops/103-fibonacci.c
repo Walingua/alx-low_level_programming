668883a456f1fd464eea8432a6c23ae64380e547
Walingua <waleade77@gmail.com>
2022-09-15 02:39:27 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - first 50 fibonacci numbers
+ * Return: always 0
+ */
+
+int main(void)
+{
+	unsigned long int a, b, c, d;
+
+		a = 0;
+		b = 1;
+		c = 0;
+		do {
+			if ((c % 2) == 0)
+			d += c;
+			c = a + b;
+			a = b;
+			b = c;
+		 while (c < 4000000);
+		 printf("%lu\n", d);
+		 return (0);
+}
