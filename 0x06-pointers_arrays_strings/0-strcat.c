4cd1b151b0db031bca57a3c053f8f740c8f05ac9
Walingua <waleade77@gmail.com>
2022-09-22 00:31:01 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates the string pointed to @src to
+ * _strcat - concatenates the string pointed to by @src 
  * the end of the string pointed to by @dest
  * @dest: String that will be appended
  * @src: String to be concantenated upon
