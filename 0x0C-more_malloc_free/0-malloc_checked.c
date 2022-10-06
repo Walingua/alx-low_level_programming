1819d91fdacab1b1bca19732217d6749a9699d19
Walingua <waleade77@gmail.com>
2022-10-06 08:48:38 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - a function that allocates memory using malloc
+ * malloc_checked - allocates memory using malloc
  * @b: memory to allocate
  *
  * Reeturn: pointer to allocated memory or normal process termination if error
